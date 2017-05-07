f3c60212ccfe8453566b48ea2db3dd3d3a8566cb
ddamik <jjgorl@hanmail.net>
2017-05-07 20:48:53 +0900

    Annotation / ApplicationContext

--- TBSpring/src/UserDaoTest.java
+++ TBSpring/src/UserDaoTest.java
@@ -1,5 +1,8 @@
 import java.sql.SQLException;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import springbook.user.dao.DaoFactory;
 import springbook.user.dao.UserDao;
 import springbook.user.domain.User;
@@ -7,7 +10,6 @@ import springbook.user.domain.User;
 public class UserDaoTest {
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-		// TODO Auto-generated method stub
 
 		/*
 		//	NConnectionMaker를 만들었다면, 아래와 같이 만들면 된다.
@@ -19,19 +21,32 @@ public class UserDaoTest {
 														//	결국 두 오브젝트 사이의 의존관계 설정 효
 		 */
 		
-		UserDao dao = new DaoFactory().userDao();
+		
+		/**
+		 *	DaoFactory처럼 @Configuration이 붙은 자바 코드를 설정정보로 사용하려면 AnnotationConfigApplicationContext를 사용한다.
+		 *	ApplicationContext의 getBean()이라는 메소드를 이용해 UserDao의 오브젝트를 가져올 수 있다.
+		 *
+		 *	getBean()의 파라미터인 "userDao"는 ApplicationContext에 등록된 빈의 이름이다.
+		 *	DaoFactory에서 @Bean이라는 애노테이션 userDao라는 메소드에 붙였기때문에 메소드가 바로 빈의 이름이 된다.
+		 *	즉, DaoFactory의 userDao() 메소드를 호출해서 그 결과를 가져온다고 생각하면 된다.
+		 */
+		
+		ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+		UserDao dao = context.getBean("userDao", UserDao.class);
+		
 		User user = new User();
 		user.setId("whiteship");
-		user.setName("ddmaik");
+		user.setName("ddamik");
 		user.setPassword("password");
 		
 		dao.add(user);
-		System.out.println(user.getId() + " 등록 성공!");
+		System.out.println(user.getId() + " 등록 완료!");
 		
 		User user2 = dao.get(user.getId());
 		System.out.println(user2.getId());
 		System.out.println(user2.getName());
 		System.out.println(user2.getPassword());
+		
 	}
 
 }
--- TBSpring/src/springbook/user/dao/DaoFactory.java
+++ TBSpring/src/springbook/user/dao/DaoFactory.java
@@ -1,13 +1,20 @@
 package springbook.user.dao;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 /**
  *	팩토리의 메소드는 UserDao 타입의 오브젝트를 어떻게 만들고,
  *	어떻게 준비시킬지 결정한다.
+ *	
+ *	@Configuration: 스프링이 빈 팩토리를 위한 오브젝트 설정을 담당하는 클래스라고 인식할 수 있다.
+ *	@Bean: 오브젝트를 만들어주는 메소드에 붙인다.
  */
 
-
+@Configuration
 public class DaoFactory {
 
+	@Bean
 	public UserDao userDao(){
 		return new UserDao(this.connectionMaker());
 	}
@@ -16,10 +23,12 @@ public class DaoFactory {
 	 *	기존에 new DConnectionMaker() 부분이 중복된 부분을 공통의 메소드로 추출하여 중복을 최소화 하였고,
 	 *	이제는 메소드 수의 상관없이 유연해졌다.
 	 */
+	@Bean
 	public AccountDao accountDao(){
 		return new AccountDao(this.connectionMaker());
 	}
 	
+	@Bean
 	public MessageDao messageDao(){
 		return new MessageDao(this.connectionMaker());
 	}
